69760f7c871047154cdf9544e78b480473b34b6e
Uroš Todorović <todorovic.urosh@gmail.com>
2023-01-17 13:04:10 +0100

    custom signin created

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,12 +1,12 @@
 import express from 'express';
-import { signup } from '../controllers/auth.js';
+import { signin, signup } from '../controllers/auth.js';
 const router = express.Router();
 
 // Create a user with
 router.post('/signup', signup);
 
 // Sign in
-router.post('/signin');
+router.post('/signin', signin);
 
 // Google Auth
 router.post('/google');
