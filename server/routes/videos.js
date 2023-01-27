3ff96c928835ee4a54ff63b90d533b31d60231d6
Uroš Todorović <todorovic.urosh@gmail.com>
2023-01-27 09:59:14 +0100

    user routes and controllers added

--- server/routes/videos.js
+++ server/routes/videos.js
@@ -1,7 +1,39 @@
 import express from 'express';
-import { test } from '../controllers/video.js';
+import {
+	addVideo,
+	addView,
+	deleteVideo,
+	getVideo,
+	random,
+	subscribed,
+	trend,
+	updateVideo,
+} from '../controllers/video.js';
+import { verifyToken } from '../jwt/verifyToken.js';
 const router = express.Router();
 
-router.get('/test', test);
+// Create a video
+router.post('/', verifyToken, addVideo);
+
+// Update a video
+router.post('/:id', verifyToken, updateVideo);
+
+// Delete a video
+router.delete('/:id', verifyToken, deleteVideo);
+
+// Get a video
+router.get('/find/:id', getVideo);
+
+// Increment views of the video
+router.put('/view/:id', addView);
+
+// Trend videos
+router.get('/trend', trend);
+
+// Random videos
+router.get('/random', random);
+
+// Subscibed
+router.get('/sub', subscribed);
 
 export default router;
