673a0a80bacc681c360d653f507894e4a8addb67
Uroš Todorović <todorovic.urosh@gmail.com>
2023-01-27 09:21:32 +0100

    user routes and controllers added

--- server/routes/users.js
+++ server/routes/users.js
@@ -7,21 +7,21 @@ const router = express.Router();
 router.put('/:id', verifyToken, updateUser);
 
 // Delete User
-router.delete('/:id', deleteUser);
+router.delete('/:id', verifyToken, deleteUser);
 
 // Get a User
 router.get('/find/:id', getUser);
 
 // Subscribe User
-router.put('/subscribe/:id', subscribe);
+router.put('/subscribe/:id', verifyToken, subscribe);
 
 // Unsubscribe User
-router.put('/unsubscribe/:id', unsubscribe);
+router.put('/unsubscribe/:id', verifyToken, unsubscribe);
 
 // Like a Video
-router.put('/like/:id', like);
+router.put('/like/:id', verifyToken, like);
 
 // Dislike a Video
-router.put('/dislike/:id', dislike);
+router.put('/dislike/:id', verifyToken, dislike);
 
 export default router;
